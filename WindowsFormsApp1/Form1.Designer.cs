5c9e30cef680fce2c70656231ffc45a01f2b9434
ws373598 <ws373598@CL-KN-R514-D10>
2024-01-29 11:44:15 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Finlay has null oily men";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(303, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Oil levels: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Improve Oil for null";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(519, 281);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Finlays oily men";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        double count = 0;
+
+        int score = 1;
+
+        int cost = 1;
+
+        int waiting = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = $"Oil levels: {score}";
+            button1.Text = $"Improve oil for {Math.Ceiling(cost * (cost * 3.5))} men";
+        }
+        private async void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (waiting == 0)
+            {
+                if (count >= 250)
+                {
+                    pictureBox1.Image = new Bitmap(Properties.Resources.musc);
+                }
+
+                await increase();
+                await waitinge();
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await clicked();
+        }
+        private async Task clicked()
+        {
+            if (count >= Math.Ceiling(cost * (cost * 3.5)))
+            {
+                count = count - Math.Ceiling(cost * (cost * 3.5));
+
+                score += 1;
+
+                cost += 1;
+
+                label2.Text = $"Oil levels: {score}";
+
+                button1.Text = $"Improve oil for {Math.Ceiling(cost * (cost * 3.5))} men";
+
+                label1.Text = $"finlay has {count} oily men!";
+            }
+        }
+        private async Task increase()
+        {
+            count += score;
+
+            label1.Text = $"finlay has {count} oily men!";
+        }
+
+        private async Task waitinge()
+        {
+            waiting = 1;
+            await Task.Delay(500);
+            waiting = 0;
+        }
+    }
+}
